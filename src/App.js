c667030905a6f911a894fffbffb704ea5877b68d
MorganeLara <morganelara.design@gmail.com>
2022-11-06 18:48:08 +0100

    added source links

--- src/App.js
+++ src/App.js
@@ -8,14 +8,35 @@ function App() {
       <div className="container">
         <Weather defaultCity="Brussels" />
         <div className="credits">
-          <a
-            href="https://github.com/MorganeLara/react-weather-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Open-source code
-          </a>{" "}
-          by Morgane Lara 🇫🇷
+          <small>
+            This project was coded by Morgane Lara and is{" "}
+            <a
+              href="https://github.com/MorganeLara/react-weather-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and
+            <a
+              href="https://app.netlify.com/sites/resplendent-crepe-28e0d1/overview"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              hosted on Netlify
+            </a>
+            <br />
+            Check out my other{" "}
+            <a
+              href="https://main--legendary-cascaron-cb1365.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              project
+            </a>
+          </small>
         </div>
       </div>
     </div>
